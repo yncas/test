82dd84373c641df18b6d9d0c9bc12e2e15e39a80
Evgeniy <yncas@yandex.ru>
2015-02-18 13:17:37 +0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,60 @@
+package ext.org.forms.changeorder.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		List<String> result = new ArrayList<String>();
+		
+		String numbers = "XXX-50;-01;XXX-100;-01;-02;-03;-04;-05;-07;-08;-10;-12;-15;-18;-19;XXX-150;-01;XXX-200;-01;-02;XXX-500;-01;$end$"; //Список обозначений объектов одинаковых типов и т.д.
+		//String numbers = "РКД5А.200.400-1600.01;-01;$end$";
+		Integer beforeNumber = null;
+		int countNumber = 0;
+		String startNumber = "<startNumber = 'null'>";
+		String numberBefore = "<numberBefore = 'null'>";
+		for(String number : numbers.split(";")) 
+		{
+			System.out.println("number = " + number + ", countNumber = " + countNumber);
+			
+			if(number.startsWith("-")) //Если элемент таблицы семейств 
+			{ 
+				int currentNumber = Integer.parseInt(number.substring(number.lastIndexOf("-") + 1));
+				if(countNumber == 1) startNumber = numberBefore;
+				if(countNumber == 0) startNumber = number;
+				if(countNumber == 0 || currentNumber - beforeNumber == 1) 
+					countNumber++;
+				else {
+					if(countNumber > 2) result.add(startNumber + " ... " + numberBefore);
+					else {
+						result.add(startNumber);
+						if(countNumber == 2) 
+							result.add(numberBefore);
+					}
+					
+					startNumber = number;
+					countNumber = 1;
+				}
+				beforeNumber = currentNumber;
+				numberBefore = number;
+			}
+			else {
+				if(countNumber != 0) {
+					if(countNumber > 2) result.add(startNumber + " ... " + numberBefore);
+					else {
+						System.out.println("startNumber = " + startNumber);
+						result.add(startNumber);
+						if(countNumber == 2) 
+							result.add(numberBefore);
+					}
+				}
+				if(! "$end$".equals(number) ) result.add(number);
+				countNumber = 0;
+			}			
+		}
+		
+		System.out.println(result);
+	}
+}
